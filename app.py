2e6398a87007d32515630f8f5fecaa4728002db9
willy wen <willywen01@gmail.com>
2023-12-24 14:27:46 +0800

    added sqlite url

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///purchase_order2.db")
 db = SQLAlchemy(app)
 
 class Order(db.Model):
